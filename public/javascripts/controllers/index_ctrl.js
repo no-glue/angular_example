9aa6011184421e81ef8342f04750f0c13846ddb3
no-glue <nikolapav1985@gmail.com>
2015-04-16 14:08:05 +0200

    name change

--- public/javascripts/controllers/index_ctrl.js
+++ public/javascripts/controllers/index_ctrl.js
@@ -1,2 +1,7 @@
 theApp.controller('IndexCtrl', ['$scope', function($scope) {
+  $scope.nameChanged = function() {
+    console.debug("name changed", $scope.person.name);
+  };
+  $scope.formSubmit = function() {
+  };
 }]);
